4fd293906c5d753ac628a111a8706386bb2f3207
Abhinav Pareek <abhinav.pareek@cygnet-digital.com>
2023-12-10 00:38:41 +0530

    Added if always for failed report generation

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -5,7 +5,7 @@ export class homePage
         clickSearchBtn: '.btn.btn-default.btn-lg',
         productTitle: 'img[title="MacBook"]',
         addtoCart: 'div[id="content"] div:nth-child(1) div:nth-child(1) div:nth-child(2) div:nth-child(2) button:nth-child(1)',
-        successMessage: 'div.alert.alert-success.alert-dismissible'
+        successMessage: 'div alert.alert-success.alert-dismissible'
     }
 
     searchProduct(productName)
